de26e35ed0608534d9d3301e57b03e21ba41b7e3
d4rkpn5rk1bk <d4rkpn5rk1bk@gmail.com>
2022-05-09 20:13:21 +0500

    true false result added

--- GeometryObjects/CircleTests.cs
+++ GeometryObjects/CircleTests.cs
@@ -8,10 +8,9 @@ namespace Geometry.Tests.GeometryObjects
     public class CircleTests
     {
         [Theory]
-        [InlineData(1, Math.PI)] // Проверка единичной окружности
-        [InlineData(2, 4 * Math.PI)] // Обычных чисел
-        [InlineData(0.0001, 0.0001 * 0.0001 * Math.PI)] // Проверка на малые числа
-        [InlineData(1000000, 1000000.0 * 1000000.0 * Math.PI)] // Проверка на большие числа
+        [InlineData(1, Math.PI)]
+        [InlineData(0.0001, 0.0001 * 0.0001 * Math.PI)]
+        [InlineData(1000000, 1000000.0 * 1000000.0 * Math.PI)]
         public void CircleSquareTheory(double radius, double square)
         {
             Circle circle = new Circle(radius);
@@ -19,19 +18,21 @@ namespace Geometry.Tests.GeometryObjects
         }
 
         [Theory]
-        [InlineData(Double.MinValue)]
-        [InlineData(Double.MaxValue)]
-        [InlineData(0)]
-        [InlineData(-1)]
-        [InlineData(-10)]
-        [InlineData(-0.0001)]
-        public void CircleExceptionsTheory(double radius)
+        [InlineData(Double.MinValue, false)]
+        [InlineData(Double.MaxValue, false)]
+        [InlineData(0, false)]
+        [InlineData(-0.0001, false)]
+        [InlineData(1, true)]
+        public void CircleExceptionsTheory(double radius, bool result)
         {
             Assert.Throws<GeometryException>(() =>
             {
                 Circle circle = new Circle(radius);
                 circle.GetSquare();
+                
             });
+
+            Assert.True(result);
         }
 
     }
--- GeometryObjects/TriangleByLinesTests.cs
+++ GeometryObjects/TriangleByLinesTests.cs
@@ -8,44 +8,54 @@ namespace Geometry.Tests.GeometryObjects
     public class TriangleByLinesTests
     {
         [Theory]
-        [InlineData(0, 0, 0)]
-        [InlineData(1, 1, 3)]
-        [InlineData(0, 2, 1)]
-        [InlineData(1, 0, 0)]
-        [InlineData(Double.MaxValue, Double.MaxValue, Double.MaxValue)]
-        [InlineData(Double.MaxValue, Double.MinValue, Double.MinValue)]
-        [InlineData(Double.MaxValue, 0, 0)]
-        [InlineData(Double.MaxValue, 0.01, 0.01)]
-        [InlineData(10050.01, Double.MinValue, 10000.01)]
-        public void CorrectTriangleExceptionTheory(double ab, double bc, double ca)
+        [InlineData(0, 0, 0, false)]
+        [InlineData(1, 1, 3, false)]
+        [InlineData(0, 2, 1, false)]
+        [InlineData(Double.MaxValue, Double.MaxValue, Double.MaxValue, false)]
+        [InlineData(Double.MaxValue, Double.MinValue, Double.MinValue, false)]
+        [InlineData(Double.MaxValue, 0, 0, false)]
+        [InlineData(Double.MaxValue, 0.01, 0.01, false)]
+        [InlineData(10050.01, Double.MinValue, 10000.01, false)]
+        [InlineData(3, 4, 5, true)]
+        [InlineData(0.01, 0.01, 0.01, true)]
+        public void CorrectTriangleExceptionTheory(double ab, double bc, double ca, bool result)
         {
-            Assert.Throws<GeometryException>(() =>
+            if (result)
             {
-                TriangleByLines triangle = new TriangleByLines(ab, bc, ca);
-            });
+                Assert.Throws<GeometryException>(() =>
+                {
+                    TriangleByLines triangle = new TriangleByLines(ab, bc, ca);
+                });
+            }
+            else
+            {
+                Assert.True(true);
+            }
+            
         }
 
         [Theory]
-        [InlineData(3, 4, 5, 6)]
-        [InlineData(0.03, 0.04, 0.05, 0.06)]
-        [InlineData(6, 8, 10, 24)]
-        [InlineData(9, 12, 15, 54)]
-        [InlineData(12, 16, 20, 96)]
-        public void TriangleSquareTheory(double ab, double bc, double ca, double square)
+        [InlineData(3, 4, 5, 6, true)]
+        [InlineData(0.03, 0.04, 0.05, 50, false)]
+        public void TriangleSquareTheory(double ab, double bc, double ca, double square, bool result)
         {
+            
             TriangleByLines triangle = new TriangleByLines(ab, bc, ca);
-            Assert.Equal(triangle.GetSquare(), square);
+
+            if (result)
+            {
+                Assert.Equal(triangle.GetSquare(), square);
+            }
+            else
+            {
+                Assert.NotEqual(triangle.GetSquare(), square);
+            }
+            
         }
 
         [Theory]
         [InlineData(4, 4, 5, false)]
-        [InlineData(7, 8, 10, false)]
-        [InlineData(10, 12, 15, false)]
-        [InlineData(13, 16, 20, false)]
         [InlineData(3, 4, 5, true)]
-        [InlineData(6, 8, 10, true)]
-        [InlineData(9, 12, 15, true)]
-        [InlineData(12, 16, 20, true)]
         public void TriangleIsStrightTheory(double ab, double bc, double ca, bool isStright)
         {
             TriangleByLines triangle = new TriangleByLines(ab, bc, ca);
